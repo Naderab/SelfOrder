56de3bccbfdef63e65b2865a438da02392fc75af
nizar04 <33844522+nizar04@users.noreply.github.com>
2020-04-14 16:05:55 +0100

    commit

---
+++ validation/profile.js
@@ -0,0 +1,30 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  data.handle = !isEmpty(data.handle) ? data.handle : '';
+  data.status = !isEmpty(data.status) ? data.status : '';
+  data.skills = !isEmpty(data.skills) ? data.skills : '';
+
+
+  if (Validator.isEmpty(data.status)) {
+    errors.status = 'Status field is required';
+  }
+
+  if (Validator.isEmpty(data.skills)) {
+    errors.skills = 'Skills field is required';
+  }
+
+  if (!isEmpty(data.website)) {
+    if (!Validator.isURL(data.website)) {
+      errors.website = 'Not a valid URL';
+    }
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
