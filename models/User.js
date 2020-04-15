517e79f82b05f09ea3224d3262f7baaaf27cf357
nizar04 <33844522+nizar04@users.noreply.github.com>
2020-04-15 18:49:06 +0100

    comit

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    activated:{
+        type: Boolean,
+        default: false,
+    },
+    secretToken:{
+        type: String
+    },
+    role:{
+        type:String,
+        required:true
+    },
+   
+});
+module.exports =  User = mongoose.model('user', UserSchema);
